8cb0857e2805dcd8123fe1c9a492d3e627b09395
Pis4ni <pis4wrk@gmail.com>
2023-09-08 15:55:20 +0200

    add: basic component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <script>
+import AppHeader from './components/Appheader.vue'
+import AppContent from './components/AppContent.vue'
   export default {
     data(){
       return {
-        title:'ciao'
+        title:'AppVue Working'
       }
-    }
+    },
+    components: {AppHeader, AppContent}
   }
-  components: {}
 </script>
 
 <template>
-  <h1>hi{{ title }}</h1>
+<main>
+  <header class="deb">
+    <AppHeader></AppHeader>
+  </header>
+  <section class="deb">
+    <AppContent></AppContent>
+  </section>
+  <h1>{{ title }}</h1>
+</main>
 </template>
 
 <style lang="scss">
-
+  body{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  .deb{
+    border: 1px dashed red;
+  }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    data(){
+      return {
+        test:'test'
+      }
+    }
+  }
+</script>
+
+<template>
+    <div>
+        <h1>
+            hi, i'm an header {{ test }}
+        </h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
