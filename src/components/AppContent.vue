254bcbf6facf95fa35c91897f394a915c17f2332
Pis4ni <pis4wrk@gmail.com>
2023-09-08 15:55:52 +0200

    add: basic component

---
+++ src/components/AppContent.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    data(){
+      return {
+        test:'test-01'
+      }
+    }
+  }
+</script>
+
+<template>
+    <div>
+        <p>
+            hi, i'm content {{ test }}            
+        </p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
