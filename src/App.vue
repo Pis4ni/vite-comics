25adbd7a227d9179cde304885b3f862f045b5adb
Pis4ni <pis4wrk@gmail.com>
2023-09-08 15:30:11 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+    data(){
+      return {
+        title:'ciao'
+      }
+    }
+  }
+  components: {}
+</script>
+
+<template>
+  <h1>hi{{ title }}</h1>
+</template>
+
+<style lang="scss">
+
+</style>
